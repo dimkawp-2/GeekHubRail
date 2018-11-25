d1b8f2972e3a324f973b584a4e9ebd16d68d9be5
dimkawp <dimkawp@gmail.com>
2018-11-25 12:51:03 +0200

    work on add scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get '/', to: 'app#app'
+  get '/', to: 'app#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
